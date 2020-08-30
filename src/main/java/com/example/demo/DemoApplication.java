59e600bc54ffaa1b503672031af7ed871cab2b3c
micmc23000 <micmc48@gmail.com>
2020-08-30 16:50:10 +0100

    working?

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        //
+        //  Press anykey to quit
+        //
+        System.out.println("Press Enter to exit.");
+        var scanner = new Scanner(System.in);
+        var key = scanner.nextLine();
+        System.exit(-1);
+    }
 
 }
